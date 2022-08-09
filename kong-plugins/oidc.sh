80b3480f94360416ee8b63911657378acc84e28b
giovani <giovanibrioni@gmail.com>
2022-08-09 01:11:07 -0300

    fix config.discovery link on oidc plugin

--- kong-plugins/oidc.sh
+++ kong-plugins/oidc.sh
@@ -5,5 +5,5 @@ curl -i -X POST http://localhost/admin-api/services/httpbin-service/plugins \
   --data config.bearer_only="yes" \
   --data config.realm="kong" \
   --data config.introspection_endpoint="http://keycloak.local/realms/kong/protocol/openid-connect/token/introspect" \
-  --data config.discovery="http://keycloack.local/auth/realms/kong/.well-known/openid-configuration"  \
+  --data config.discovery="http://keycloack.local/realms/kong/.well-known/openid-configuration"  \
   --data config.scope="openid"
