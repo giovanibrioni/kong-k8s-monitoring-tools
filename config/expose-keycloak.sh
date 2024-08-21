5b481ecfe0d508cc1c82a56c2a96e8beb45e1ee1
giovani <giovanibrioni@gmail.com>
2024-08-21 12:42:03 -0300

    change config host

--- config/expose-keycloak.sh
+++ config/expose-keycloak.sh
@@ -1,9 +1,9 @@
 curl -i -X POST \
-  --url http://localhost/admin-api/services/ \
+  --url http://localhost:8001/services/ \
   --data 'name=keycloak-service' \
   --data 'url=http://keycloak-kong.keycloak.svc.cluster.local:8080/'
 
 curl -i -X POST \
-  --url http://localhost/admin-api/services/keycloak-service/routes/ \
+  --url http://localhost:8001/services/keycloak-service/routes/ \
   --data 'hosts[]=keycloak.local' \
   --data name=keycloak-route
